f83db8c1f4ca2d8f64961ebbce98763ca1563395
ninatxb <69555706+ninatxb@users.noreply.github.com>
2020-08-17 18:30:15 -0400

    add import rs_bot and other stuff

--- server.py
+++ server.py
@@ -1,5 +1,12 @@
 from os import environ
 from flask import Flask
+import rs_bot
 
 app = Flask(__name__)
+
+@app.route("/")
+def home():
+    bot.tweet_quote()
+    return "tweeting a radio silence quote..."
+
 app.run(host= "0.0.0.0", port=environ.get("PORT"))
